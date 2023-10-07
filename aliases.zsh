fcb8c2fa75c12be2c659e6d5f98f89e4205665b3
Peng Liu <littlenewton6@gmail.com>
2023-10-08 04:08:17 +0800

    [+] Add aliases collection.

---
+++ aliases.zsh
@@ -0,0 +1,12 @@
+alias     pip='pip3'
+alias ipython='ipython3'
+
+alias      ll='ls -lFh'
+alias      la='ls -a1'
+alias       l='ls -1'
+alias    grep='grep  --color=auto'
+alias   fgrep='fgrep --color=auto'
+alias   egrep='egrep --color=auto'
+alias     dir='ls'
+alias      ip='ip -color'
+alias    dirs='dirs -lpv'
