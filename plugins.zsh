2aa204f95aefe3ac66cd3d2a9c9bb02c9e5b8d7b
Peng Liu <littlenewton6@gmail.com>
2023-10-09 11:15:38 +0800

    [*] Change zim plugins script.

--- plugins.zsh
+++ plugins.zsh
@@ -1,6 +1,10 @@
-export PLUG_DIR=$HOME/.local/share/zim/modules/
-if [[ ! -d $PLUG_DIR ]]; then
-	  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-    rm ~/.zimrc
-    ln -s ~/.config/zsh/zimrc ~/.zimrc
+# Download zimfw plugin manager if missing.
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
+
+# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init -q
 fi
