1e100df960eeffd06116f32d4eabc6056361a189
Peng Liu <littlenewton6@gmail.com>
2023-10-10 09:29:07 +0800

    [*] Combine oh-my-zsh and zim init.

--- .zshrc
+++ .zshrc
@@ -20,7 +20,6 @@ source ${XDG_CONFIG_HOME}/zsh/functions/init_env.zsh
 source ${XDG_CONFIG_HOME}/zsh/functions/init_alias.zsh
 source ${XDG_CONFIG_HOME}/zsh/functions/init_plugin.zsh
 source ${XDG_CONFIG_HOME}/zsh/functions/init_mapping.zsh
-source ${XDG_CONFIG_HOME}/zsh/functions/init_ohmyzsh.zsh
 source ${XDG_CONFIG_HOME}/zsh/functions/init_conda.zsh
 source ${XDG_CONFIG_HOME}/zsh/functions/init_latex.zsh
 source ${XDG_CONFIG_HOME}/zsh/functions/os_update.zsh
--- functions/init_plugin.zsh
+++ functions/init_plugin.zsh
@@ -1,3 +1,25 @@
+init_ohmyzsh() {
+    # Check if `XDG_DATA_HOME` is defined
+    if [[ -z "${XDG_DATA_HOME}" ]]; then
+        echo "ERROR: XDG_DATA_HOME is not defined."
+        return 1
+    fi
+
+    # check if ${XDG_DATA_HOME}/oh-my-zsh exists
+    if [[ -d "${XDG_DATA_HOME}/oh-my-zsh" ]]; then
+        return 0
+    else
+        echo "${XDG_DATA_HOME}/oh-my-zsh not exists,"
+        echo "now clone oh-my-zsh to ${XDG_DATA_HOME}/oh-my-zsh."
+        git clone https://github.com/ohmyzsh/ohmyzsh.git "${XDG_DATA_HOME}/oh-my-zsh"
+        echo "clone oh-my-zsh done."
+    fi
+}
+
+init_ohmyzsh;
+
+
+
 # Download zimfw plugin manager if missing.
 if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
     curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
@@ -11,11 +33,15 @@ fi
 
 source ${ZIM_HOME}/init.zsh
 
-zmodload -F zsh/terminfo +p:terminfo
 
 # Bind ^[[A/^[[B manually so up/down works both before and after zle-line-init
+zmodload -F zsh/terminfo +p:terminfo
 for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
 for key ('^[[B' '^N' ${terminfo[kcud1]}) bindkey ${key} history-substring-search-down
 for key ('k') bindkey -M vicmd ${key} history-substring-search-up
 for key ('j') bindkey -M vicmd ${key} history-substring-search-down
 unset key
+
+
+# Init Oh-My-Zsh at the last step.
+source ${ZSH}/oh-my-zsh.sh
