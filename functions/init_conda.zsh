65832dd22eaea03e4dd44ed89da487d4bbded208
Peng Liu <littlenewton6@gmail.com>
2023-10-11 01:13:48 +0800

    [+] Add macOS support for conda.

--- functions/init_conda.zsh
+++ functions/init_conda.zsh
@@ -41,6 +41,25 @@ set_conda() {
 
 
 
+    elif [[ $os_type == "macOS" ]]; then
+
+        # >>> conda initialize >>>
+        # !! Contents within this block are managed by 'conda init' !!
+        __conda_setup="$('/Users/newton/bin/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+        if [ $? -eq 0 ]; then
+            eval "$__conda_setup"
+        else
+            if [ -f "/Users/newton/bin/miniconda3/etc/profile.d/conda.sh" ]; then
+                . "/Users/newton/bin/miniconda3/etc/profile.d/conda.sh"
+            else
+                export PATH="/Users/newton/bin/miniconda3/bin:$PATH"
+            fi
+        fi
+        unset __conda_setup
+        # <<< conda initialize <<<
+
+
+
     else
         echo "Unsupported OS type: $os_type"
     fi
