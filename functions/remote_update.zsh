acc56df004b061b800828e3f61c80034f3f44ee8
Peng Liu <littlenewton6@gmail.com>
2024-04-08 15:24:59 +0800

    [*] Change `ServerInfo` data structure.

--- functions/remote_update.zsh
+++ functions/remote_update.zsh
@@ -1,24 +1,25 @@
 function remote-update() {
-    typeset -A ServerInfo=(
-        ["5820-debian"]="ip:10.1.2.21; port:21221"
-        ["4950-debian"]="ip:10.2.2.21; port:22221"
-        ["r740-debian"]="ip:10.2.4.21; port:22421"
-        ["z690-debian"]="ip:10.2.5.21; port:22521"
-        ["x1e-debian"]="ip:10.2.6.21; port:22621"
-        ["x299-debian"]="ip:10.2.7.21; port:22721"
+    ServerInfo=(
+        "hostname:5820-debian; ip:10.1.2.21; port:21221"
+        "hostname:4950-debian; ip:10.2.2.21; port:22221"
+        "hostname:r740-debian; ip:10.2.4.21; port:22421"
+        "hostname:z690-debian; ip:10.2.5.21; port:22521"
+        "hostname:x1e-debian;  ip:10.2.6.21; port:22621"
+        "hostname:x299-debian; ip:10.2.7.21; port:22721"
     )
 
     local localUsername="newton"
 
-    for server in ${(k)ServerInfo}; do
-        serverName=$server
-        serverHost=$(echo $ServerInfo[$server] | awk -F'[:;]' '{print $2}')
-        serverPort=$(echo $ServerInfo[$server] | awk -F'[:;]' '{print $4}')
+    for server in "${ServerInfo[@]}"; do
+        # Using awk to extract each part of the server info
+        serverName=$(echo $server | awk -F'[:;]' '{print $2}' | xargs)
+        serverAddr=$(echo $server | awk -F'[:;]' '{print $4}' | xargs)
+        serverPort=$(echo $server | awk -F'[:;]' '{print $6}' | xargs)
 
         # Upgrade
         echo "===================================== ↓ ↓ ↓ =====================================" | lolcat
         echo "$serverName is getting updated" | lolcat
-        ssh "$localUsername@$serverHost" "-p $serverPort" "source ~/.config/zsh/.zshrc && os-update"
+        ssh "$localUsername@$serverAddr" "-p $serverPort" "source ~/.config/zsh/.zshrc && os-update"
         echo "$serverName upgrading done" | lolcat
         echo ""
     done
