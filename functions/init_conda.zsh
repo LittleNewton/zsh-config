b3dea5dc3ef4fab46badc831934110bae789f989
Peng Liu <littlenewton6@gmail.com>
2025-01-01 17:26:57 +0000

    [*] Correct wrong CONDA path.
    Change it from absolute path to relative path.
    
    Now it can work in root environment.

--- functions/init_conda.zsh
+++ functions/init_conda.zsh
@@ -1,75 +1,33 @@
 set_conda() {
-    if [[ $os_type == "TrueNAS_SCALE" ]]; then
-
-
-
-        # >>> conda initialize >>>
-        # !! Contents within this block are managed by 'conda init' !!
-        __conda_setup="$('/mnt/DapuStor_R5100_RAID-Z1/newton/bin/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-        if [ $? -eq 0 ]; then
-            eval "$__conda_setup"
-        else
-            if [ -f "/mnt/DapuStor_R5100_RAID-Z1/newton/bin/miniconda3/etc/profile.d/conda.sh" ]; then
-                . "/mnt/DapuStor_R5100_RAID-Z1/newton/bin/miniconda3/etc/profile.d/conda.sh"
+    case $os_type in
+        "Debian" | "Ubuntu" | "Manjaro" | "TrueNAS_SCALE" | "macOS")
+            # >>> conda initialize >>>
+            # !! Contents within this block are managed by 'conda init' !!
+            __conda_setup=$("$HOME/bin/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)
+            if [ $? -eq 0 ]; then
+                eval "$__conda_setup"
             else
-                export PATH="/mnt/DapuStor_R5100_RAID-Z1/newton/bin/miniconda3/bin:$PATH"
+                if [ -f "$HOME/bin/miniconda3/etc/profile.d/conda.sh" ]; then
+                    . "$HOME/bin/miniconda3/etc/profile.d/conda.sh"
+                else
+                    export PATH="$HOME/bin/miniconda3/bin:$PATH"
+                fi
             fi
-        fi
-        unset __conda_setup
-        # <<< conda initialize <<<
-
-
-
-
-    elif [[ $os_type == "Debian" || $os_type == "Ubuntu" || $os_type == "Manjaro" ]]; then
-
-
-
-        # >>> conda initialize >>>
-        # !! Contents within this block are managed by 'conda init' !!
-        __conda_setup="$('/home/newton/bin/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-        if [ $? -eq 0 ]; then
-            eval "$__conda_setup"
-        else
-            if [ -f "/home/newton/bin/miniconda3/etc/profile.d/conda.sh" ]; then
-                . "/home/newton/bin/miniconda3/etc/profile.d/conda.sh"
-            else
-                export PATH="/home/newton/bin/miniconda3/bin:$PATH"
-            fi
-        fi
-        unset __conda_setup
-        # <<< conda initialize <<<
-
-
-
-    elif [[ $os_type == "macOS" ]]; then
-
-
-
-        # >>> conda initialize >>>
-        # !! Contents within this block are managed by 'conda init' !!
-        __conda_setup="$('/Users/newton/bin/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-        if [ $? -eq 0 ]; then
-            eval "$__conda_setup"
-        else
-            if [ -f "/Users/newton/bin/miniconda3/etc/profile.d/conda.sh" ]; then
-                . "/Users/newton/bin/miniconda3/etc/profile.d/conda.sh"
-            else
-                export PATH="/Users/newton/bin/miniconda3/bin:$PATH"
-            fi
-        fi
-        unset __conda_setup
-        # <<< conda initialize <<<
-
-
-
-    elif [[ $os_type == "OpenWrt" ]]; then
-        # Just do nothing because OpenWrt don't need Anaconda.
-
-
-
-    else
-        echo "Unsupported OS type: $os_type"
-    fi
+            unset __conda_setup
+            # <<< conda initialize <<<
+            ;;
+        
+        "OpenWrt")
+            # OpenWrt 不需要 Anaconda 配置
+            # No action needed
+            ;;
+        
+        *)
+            echo "Unsupported OS type: $os_type"
+            ;;
+    esac
 }
+
+# 执行函数
 set_conda;
+
