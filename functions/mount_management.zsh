194b186a7d9dccaae1b06f3aefb7247d6c3a24f9
Peng Liu <littlenewton6@gmail.com>
2025-01-22 03:29:57 +0000

    [+] mount: add `git_repo` nfs entry.

--- functions/mount_management.zsh
+++ functions/mount_management.zsh
@@ -3,6 +3,7 @@ mount_nfs() {
     echo "Mounting NFS shares..."
     sudo mount /mnt/DapuStor_R5100_RAID-Z1/app_data
     sudo mount /mnt/DapuStor_R5100_RAID-Z1/Documents
+    sudo mount /mnt/DapuStor_R5100_RAID-Z1/git_repo
     sudo mount /mnt/DapuStor_R5100_RAID-Z1/Software
     sudo mount /mnt/Toshiba_MG06S_RAID-Z1/Downloads
     sudo mount /mnt/WD_HC550_RAID-Z1/Media
@@ -14,6 +15,7 @@ unmount_nfs() {
     echo "Unmounting NFS shares..."
     sudo umount /mnt/DapuStor_R5100_RAID-Z1/app_data
     sudo umount /mnt/DapuStor_R5100_RAID-Z1/Documents
+    sudo umount /mnt/DapuStor_R5100_RAID-Z1/git_repo
     sudo umount /mnt/DapuStor_R5100_RAID-Z1/Software
     sudo umount /mnt/Toshiba_MG06S_RAID-Z1/Downloads
     sudo umount /mnt/WD_HC550_RAID-Z1/Media
