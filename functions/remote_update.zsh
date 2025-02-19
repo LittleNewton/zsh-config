bf7cf13997b5a1092b3184f3e52906219e354d36
Peng Liu <littlenewton6@gmail.com>
2025-02-19 08:48:45 +0000

    [*] Support read server info from local ssh config.

--- functions/remote_update.zsh
+++ functions/remote_update.zsh
@@ -1,12 +1,12 @@
 # 全局服务器信息
 ServerInfo=(
-    "hostname:5820-debian; ip:10.1.2.21; port:21221"
-    "hostname:4950-debian; ip:10.2.2.21; port:22221"
-    "hostname:r740-debian; ip:10.2.4.21; port:22421"
-    "hostname:z690-debian; ip:10.2.5.21; port:22521"
-    "hostname:x1e-debian;  ip:10.2.6.21; port:22621"
-    "hostname:x299-debian; ip:10.2.7.21; port:22721"
-    "hostname:gtr7-debian; ip:10.4.1.21; port:24121"
+    5820-debian
+    4950-debian
+    r740-debian
+    z690-debian
+    x1e-debian
+    x299-debian
+    gtr7-debian
 )
 
 function remote-exe() {
@@ -21,14 +21,11 @@ function remote-exe() {
 
     for server in "${ServerInfo[@]}"; do
         # Using awk to extract each part of the server info
-        serverName=$(echo "$server" | awk -F'[:;]' '{print $2}' | xargs)
-        serverAddr=$(echo "$server" | awk -F'[:;]' '{print $4}' | xargs)
-        serverPort=$(echo "$server" | awk -F'[:;]' '{print $6}' | xargs)
 
         echo "===================================== ↓ ↓ ↓ =====================================" | lolcat
-        echo "Run command '$command' on $serverName" | lolcat
-        ssh -p "$serverPort" "$localUsername@$serverAddr" "$command"
-        echo "Command on $serverName is done." | lolcat
+        echo "Run command '$command' on $server" | lolcat
+        ssh "$server" "$command"
+        echo "Command on $server is done." | lolcat
         echo ""
     done
 
