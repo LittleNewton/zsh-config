8a5b0a30ae6bd36e16becfc72c47d04e1dbbe33c
Peng Liu <littlenewton6@gmail.com>
2026-05-17 17:39:26 +0800

    fix: adjust pane height in r740-debian tmux script

--- tmux/r740-debian.sh
+++ tmux/r740-debian.sh
@@ -19,7 +19,7 @@ zpool_pane=$(tmux split-window -h -p 33 -c "$HOME" -t "$shell_pane" -P -F "#{pan
 tmux resize-pane -t "$shell_pane" -x 127
 
 left_iotop_pane=$(tmux split-window -v -p 34 -c "$HOME" -t "$shell_pane" -P -F "#{pane_id}")
-tmux resize-pane -t "$shell_pane" -y 34
+tmux resize-pane -t "$shell_pane" -y 33
 
 right_iotop_pane=$(tmux split-window -v -p 78 -c "$HOME" -t "$zpool_pane" -P -F "#{pane_id}")
 tmux resize-pane -t "$zpool_pane" -y 11
